1f590e4140dca8ee1a946c551efbc2f33d76c9b9
Yosmarli Parica <101285117+Yosmarli@users.noreply.github.com>
2023-03-06 12:49:49 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const nombre = "Yosmarli"
+const apellido = "Parica"
+
+
+const datos = {
+    nombre,
+    apellido
+}
+
+sessionStorage.setItem("datos", JSON.stringify(datos))
+localStorage.setItem("datos", JSON.stringify(datos))
+
+
+const now = new Date()
+
+document.cookie = `datos=${JSON.stringify(datos)};expires=${new Date(now.getTime() + 2 * 60000)}`
